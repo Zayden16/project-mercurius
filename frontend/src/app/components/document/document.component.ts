3794226bfb84c187cdc9541509514ba87abc0c04
Yazdan Musa <yazdanmusa@gmail.com>
2021-06-09 13:44:59 +0200

    PM-16: temp

--- frontend/src/app/components/document/document.component.ts
+++ frontend/src/app/components/document/document.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { DocumentService } from 'src/app/services/document.service';
+import { UserService } from 'src/app/services/user.service';
 import { Document } from 'src/model/Document';
+import { User } from 'src/model/User';
 
 @Component({
   selector: 'app-document',
@@ -9,14 +13,20 @@ import { Document } from 'src/model/Document';
 export class DocumentComponent implements OnInit {
 
   documents: Document[] = [];
+  users: User[] = [];
+  creators: SelectItem[] = [];
   clonedDocuments: any;
   newDocument =  {} as Document; 
-
   displayDialog: boolean = false;
 
-  constructor() { }
+  constructor(private docService: DocumentService, private userService: UserService) { }
 
   async ngOnInit(): Promise<void> {
+    this.documents = await this.docService.getDocuments();
+    this.users = await this.userService.getUsers();
+    this.users.forEach(user => {
+      this.creators.push({label: user.User_FirstName + user.User_LastName, value: user.User_Id});
+    });
   }
 
   showDialog(){
--- frontend/src/model/Document.ts
+++ frontend/src/model/Document.ts
@@ -1,9 +1,9 @@
 export interface Document {
-    document_Id: number,
-    document_Number: number,
-    document_TypeId: number,
-    document_CreatorId: number,
-    document_SendeeId: number,
-    document_StatusId: number,
-    document_ArticlePositionId: number
+    Document_Id: number,
+    Document_Number: number,
+    Document_TypeId: number,
+    Document_CreatorId: number,
+    Document_SendeeId: number,
+    Document_StatusId: number,
+    Document_ArticlePositionId: number
   }
