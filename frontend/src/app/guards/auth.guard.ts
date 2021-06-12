7547d16fbdd2076c823890ea317b826fcaee5569
Yazdan Musa <yazdanmusa@gmail.com>
2021-06-12 22:33:58 +0200

    PM-16: Fix Login

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     if (currentUser) {
         return true;
     }
-
+    
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
