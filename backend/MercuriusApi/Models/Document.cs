668009921c55fc30eef763be5bf23f8c538d82f8
Raphael Härtel <raphael.haertel@bluewin.ch>
2021-06-13 15:50:42 +0200

    Pm-16: Implement JsonPropertyName

--- backend/MercuriusApi/Models/Document.cs
+++ backend/MercuriusApi/Models/Document.cs
@@ -1,19 +1,28 @@
-﻿namespace MercuriusApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MercuriusApi.Models
 {
     public class Document
     {
+        [JsonPropertyName("Id")]
         public int Document_Id { get; set; }
 
+        [JsonPropertyName("Number")]
         public int Document_Number { get; set; }
 
+        [JsonPropertyName("TypeId")]
         public int Document_TypeId { get; set; }
 
+        [JsonPropertyName("CreatorId")]
         public int Document_CreatorId { get; set; }
 
+        [JsonPropertyName("SendeeId")]
         public int Document_SendeeId { get; set; }
 
+        [JsonPropertyName("StatusId")]
         public int Document_StatusId { get; set; }
 
+        [JsonPropertyName("ArticlePositionId")]
         public int Document_ArticlePositionId { get; set; }
     }
 }
--- backend/MercuriusApi/Models/User.cs
+++ backend/MercuriusApi/Models/User.cs
@@ -4,19 +4,19 @@ namespace MercuriusApi.Models
 {
     public class User
     {
-        [JsonPropertyName("User_Id")]
+        [JsonPropertyName("Id")]
         public int User_Id { get; set; }
-        [JsonPropertyName("User_FirstName")]
 
+        [JsonPropertyName("FirstName")]
         public string User_FirstName { get; set; }
-        [JsonPropertyName("User_LastName")]
 
+        [JsonPropertyName("LastName")]
         public string User_LastName { get; set; }
-        [JsonPropertyName("User_DisplayName")]
 
+        [JsonPropertyName("DisplayName")]
         public string User_DisplayName { get; set; }
-        [JsonPropertyName("User_Mail")]
 
+        [JsonPropertyName("Mail")]
         public string User_Mail { get; set; }
 
         [Newtonsoft.Json.JsonIgnore]
